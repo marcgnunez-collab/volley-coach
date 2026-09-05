033803a54a3909014ed56691ccf053fcb27326d8
marcgnunez-collab <marcgnunez@gmail.com>
2026-09-05 16:36:17 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'volley-coach-v5.5-183-ejercicios-propios-pdf-historial';
+const CACHE_NAME='volley-coach-v55-catalogo-completo-691';
 const APP_SHELL = ['./','./index.html','./manifest.webmanifest','./apple-touch-icon.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
